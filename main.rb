2445387b39fad06ddc1b0db6b87d3d5aaec00e4b
Namrone <151798569+Namrone@users.noreply.github.com>
2024-07-31 17:52:36 -0700

    Changed logic on change_hints_color and game_end_test and some more places to make the game running for if a player is guessing

--- main.rb
+++ main.rb
@@ -1,23 +1,40 @@
 require 'colorize'
 
 class Mastermind
-  attr_reader :board, :hints, :template, :colors, :empty_key, :answer_key, :player, :color_key
+  attr_reader :board, :hints, :template, :colors, :empty_key, :answer_key, :player, :color_key, :letter_key, :who_player, :game_finished, :current_row, :key
 
   def initialize
-    @board = Array.new(12,['X','X','X','X'])
-    @hints = Array.new(12,['|','|','|','|'])
+    @board = Array.new(12){['X','X','X','X']}
+    @hints = Array.new(12){Array.new(4)}
     @template = ['O'.colorize(:red) + ' = "R"', 'O'.colorize(:green) + ' = "G"', 'O'.colorize(:blue) + ' = "B"', 'O'.colorize(:yellow) + ' = "Y"', 'O'.colorize(:magenta) + ' = "M"', 'O'.colorize(:cyan) + ' = "C"', 'O'.colorize(:black) + ' = "BLK"', 'O'.colorize(:white) + ' = "W"', "|".colorize(:red) + ' = Correct color but wrong position', '|'.colorize(:green) + ' = Correct color and position']
     @colors = ['R', 'G', 'B', 'Y', 'M', 'C', 'BLK', 'W']
     @empty_key = Array.new(4)
     @answer_key = Array.new(4)
+    @key = {'R' => 'O'.colorize(:red), 'G' => 'O'.colorize(:green), 'B' => 'O'.colorize(:blue),'Y' => 'O'.colorize(:yellow),'M' => 'O'.colorize(:magenta),'C' => 'O'.colorize(:cyan),'BLK' => 'O'.colorize(:black),'W' => 'O'.colorize(:white)}
     @player = nil
     @color_key = Array.new(4)
+    @letter_key = Array.new(4)
+    @who_player = nil
+    @game_finished = false
+    @current_row = 0
   end
 
-  def get_key(player)
-    if player
+  def which_player
+    while @who_player == nil do 
+      puts "Type ('Y'/'y') if you'd like to be the one guessing and ('N'/'n') if you want to choose the 4 codes"
+      choice = gets.chomp.upcase
+      if choice == 'Y'
+        @who_player = true
+      elsif choice == 'N'
+        @who_player = false
+      end
+    end
+  end
+
+  def get_answer_key
+    if !@who_player
       @answer_key = @empty_key.each_with_index.map do |peg_color, index|
-        while !colors.include?(peg_color) do
+        while !@colors.include?(peg_color) do
           p "Enter the color you'd like for peg position ##{index+1}. Follow key on right side for colors"
           peg_color = gets.chomp.upcase
         end
@@ -25,44 +42,87 @@ class Mastermind
       end
     else
       @answer_key = @empty_key.map do
-        random_color = colors[rand(8)]
+        random_color = @colors[rand(8)]
         random_color
       end
     end
   end
 
   def get_guess
-    @empty_key.each_with_index do |peg, index|
-    puts "What is your current guess of colors. Separate each guess by a comma. See color key on the right."
-    
-  end
-
-  def convert_key
-    key = {'R' => 'O'.colorize(:red), 'G' => 'O'.colorize(:green), 'B' => 'O'.colorize(:blue),'Y' => 'O'.colorize(:yellow),'M' => 'O'.colorize(:magenta),'C' => 'O'.colorize(:cyan),'BLK' => 'O'.colorize(:black),'W' => 'O'.colorize(:white)}
-
-    @color_key = letter_key.map do |letter|
-      key[letter]
+    @letter_key = @empty_key.each_with_index.map do |peg, index| 
+      while peg == nil do
+        puts "What is your current guess of colors on peg #{index+1}. Please type the letter corresponding to the color of your choice (see color key)."
+        peg = gets.chomp.upcase
+        @colors.include?(peg) ?  peg : peg = nil
+      end
+      peg
     end
   end
 
+  def game_end_test
+    if @letter_key == @answer_key
+      puts "\nCongrats you won!"
+      @game_finished = true
+    elsif @current_row == 12
+      colored_answer = @answer_key.map do |letter|
+        @key[letter]
+      end
+      puts "\nYou lost, better luck next time! The correct answer was: #{colored_answer.join(' ')}"
+      @game_finished = true
+    end
+  end
+
+  def convert_key # <= converts the letter array into one with 'O' with the corresponding color
+    @color_key = @letter_key.map do |letter|
+      @key[letter]
+    end
+  end
+
+  def change_hints_color
+    temp_answer = @answer_key.clone
+    changed_key = Array.new(4)
+    changed_key = @letter_key.each_with_index.map do |peg, index|
+      if peg == @answer_key[index]
+        @hints[@current_row].push('|'.colorize(:green))
+        temp_answer.delete_at(temp_answer.index(peg)) #<- removes color from the array it is being compared to so if mulitple same color is guessed but only answer key only has 1 guess then it won't change all the pegs to red
+        peg = ''
+      end
+      peg
+    end
+    changed_key.each_with_index.map do |peg, index| #<- finds if there's any colors in the guess & answer key but not in the right location
+      if temp_answer.include?(peg)
+        @hints[@current_row].push('|'.colorize(:red))
+        temp_answer.delete_at(temp_answer.index(peg))
+      end
+    end
+  end
+
+  def add_key_to_board
+    @board[@current_row] = @color_key
+    @current_row += 1
+  end
+
   def print_board 
+    puts "\n", @template
     @board.each_with_index do |row, index|
-      puts "Guess ##{index+1} ==> #{row} ++ #{@hints[index]}  || #{@template[index]}"
+      puts "Guess ##{index+1} ==> #{row.join(' ')} ++#{@hints[index].join(' ')}"
+    end
+  end
+
+  def play_game
+    which_player
+    puts "\n", @template
+    get_answer_key
+    while !@game_finished
+      get_guess
+      convert_key
+      change_hints_color
+      add_key_to_board
+      game_end_test
+      print_board
     end
   end
 end
 
 game = Mastermind.new()
-
-who_player = nil
-while who_player == nil do 
-  puts "Type ('Y'/'y') if you'd like to be the one guessing and ('N'/'n') if you want to choose the 4 codes"
-  choice = gets.chomp.upcase
-  if choice == 'Y'
-    who_player = true
-  elsif choice == 'N'
-    who_player = false
-  end
-end
-
-game.get_key(who_player)
+game.play_game
